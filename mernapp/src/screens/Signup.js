65ab7a42ec5e70981ecbbc3b79e122f971fd9453
SchrodingerrCat <amazingishika2@gmail.com>
2023-12-25 19:45:54 +0530

    Committed sign up and login

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Signup() {
 
-    const [credentials,setcredentials] = useState({name:"",email:"",password:"",geolocation:"",})
+    const [credentials,setcredentials] = useState({name:"",email:"",password:"",geolocation:""})
 
     const handleSubmit = async(e)=>{
         e.preventDefault();
         const response = await fetch("http://localhost:5000/api/createuser",{
             method:'POST',
             headers:{
-                'Accept': '*/*',
-                'User-Agent':"Thunder Client (https://www.thunderclient.com)"
+                'Content-Type':'application/json'
+                // 'Accept': '*/*',
+                // 'User-Agent':"Thunder Client (https://www.thunderclient.com)"
             },
 
             body: JSON.stringify( {name:credentials.name, email:credentials.email, password:credentials.password, location: credentials.geolocation})
-        });
+        })
         const json = await response.json()
         console.log(json);
 
@@ -38,7 +39,7 @@ export default function Signup() {
         </div>
         <div className="mb-3">
             <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
-            <input type="email" className="form-text" name='email' value={credentials.email} onChange={onChange} id='exampleInputEmail' />
+            <input type="email" className="form-control" name='email' value={credentials.email} id="exampleInputEmail1" onChange={onChange} />
             <div id="emailHelp" className='form-text'>We'll never share your email with anyone else</div>
         </div>
         <div className="mb-3"> 
@@ -46,7 +47,7 @@ export default function Signup() {
             <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
         </div>
         <div className="mb-3">
-            <label htmlFor="exampleInputAddress" className="form-label">Address</label>
+            <label htmlFor="exampleInputPassword1" className="form-label">Address</label>
             <input type="text" className="form-control" name='geolocation' value={credentials.geolocation} onChange={onChange} id="exampleInputAddress" />
         </div>
         <button type="submit" className="m-3 btn btn-success">Submit</button>
