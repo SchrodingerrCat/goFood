6ff021ea584b1a034567036deac1d9d596e81755
SchrodingerrCat <amazingishika2@gmail.com>
2024-01-01 15:36:46 +0530

    Added logout and my cart functionality on navbar.js

--- mernapp/src/components/Card.js
+++ mernapp/src/components/Card.js
@@ -10,7 +10,7 @@ export default function Card(props) {
         <div>
             <div>
                 <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img className="card-img-top" src={props.imgSrc} alt="Card image cap" />
+                    <img className="card-img-top" src={props.imgSrc} alt="Card image cap" style={{height:"120px",objectFit:"fill"}} />
                     <div className="card-body">
                         <h5 className="card-title">{props.foodName}</h5>
                         <div className='container w-100'>
